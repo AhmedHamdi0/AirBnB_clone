7a30f4365917cc7d5f3a5bfee2d1dfc174ae9eac
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-10 20:36:31 +0200

    implement `state` class

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines the State class."""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """A class representing a state."""
+
+    name: str = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new State object."""
+        super().__init__(*args, **kwargs)
