cdd9f10cb1b92fa682ef68418d065e46771b016b
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-10 20:52:54 +0200

    test `BaseModel` class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+import unittest
+
+from models.base_model import BaseModel
+
+
+class TestBaseModelInstantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
