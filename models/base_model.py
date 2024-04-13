6441479d80c31403ad373f88e7388d72df3376b8
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-14 00:14:55 +0200

    update the `BaseModel` class

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Define Base Model Class."""
+
 import uuid
 from datetime import datetime
 
@@ -9,23 +11,25 @@ import models
 class BaseModel:
     """Base class for models with unique IDs and timestamps."""
 
-    def __init__(self, *args, **kwargs) -> None:
-        """
-        Initialize a BaseModel instance with a unique
-        identifier and timestamps.
-        """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = self.created_at
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, tform)
+                else:
+                    self.__dict__[key] = value
+        else:
             models.storage.new(self)
-            return
-
-        self.__create_instance_from_dict(*args, **kwargs)
 
     def __str__(self) -> str:
         """Return a string representation of the BaseModel instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         """Update the 'updated_at' timestamp to the current time."""
@@ -38,19 +42,7 @@ class BaseModel:
         with class name and timestamps.
         """
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
         return obj_dict
-
-    def __create_instance_from_dict(self, *args, **kwargs) -> None:
-        """
-        Create an instance of the class using a dictionary
-        of key-value pairs.
-        """
-        for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            if key in ['created_at', 'updated_at']:
-                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-            setattr(self, key, value)
