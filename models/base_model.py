b0849f0956046778df4fb898d462f27b6c5a1d60
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-14 02:28:33 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,18 +12,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at':
-                    self.__dict__.update({'created_at':
-                                          datetime.fromisoformat(value)})
-                elif key == 'updated_at':
-                    self.__dict__.update({'updated_at':
-                                          datetime.fromisoformat(value)})
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__.update({key: value})
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -32,22 +30,18 @@ class BaseModel:
 
     def __str__(self) -> str:
         """Return a string representation of the BaseModel instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Update the 'updated_at' timestamp to the current time."""
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self) -> dict:
-        """
-        Return a dictionary representation of the BaseModel instance
-        with class name and timestamps.
-        """
-        obj_dict = {}
-        obj_dict.update({'__class__': self.__class__.__name__})
-        obj_dict.update(self.__dict__)
-        obj_dict.update({'created_at': self.created_at.isoformat(),
-                         'updated_at': self.updated_at.isoformat()})
+        """returns a dictionary containing all keys/values of __dict__"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = type(self).__name__
+        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         return obj_dict
